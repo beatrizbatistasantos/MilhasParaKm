c73dc56773a79dcf231611aa78abfadebc4744dc
Beatriz Batista <byah24@outlook.com>
2025-03-28 15:53:03 -0300

    Programa Finalizado, amém

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+double milhas = 0.0, km = 0;
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("Digite a medida em Milhas:");
+milhas = Convert.ToDouble(Console.ReadLine()!);
+
+km = milhas *  1.60934;
+
+Console.Write($"O valor de milhas convertido é: {km} KM");
+
+Console.ResetColor();
+
